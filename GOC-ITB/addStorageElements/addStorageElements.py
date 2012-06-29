43c8e259564ae861b735385828a0f05ed2483759
gfactory <gfactory@glidein-itb.grid.iu.edu>
2012-06-29 05:30:01 +0000

    most of add SE is done

--- GOC-ITB/addStorageElements/addStorageElements.py
+++ GOC-ITB/addStorageElements/addStorageElements.py
@@ -1,158 +1,241 @@
-#!/bin/env python
+#!/usr/bin/env python
 
 import config
-import elementtree.ElementTree as ET
+#import elementtree.ElementTree as ET
+import os
+import sys
 
-def addElement(elementName,elementValue,elementParent):
-    newElement = ET.SubElement(
-        elementParent,"attr",
-        name=elementName,
-        const="True",
-        glidein_publish="True",
-        job_publish="True",
-        parameter="True",
-        publish="True",
-        type="string",
-        value=elementValue)
+STARTUP_DIR="/home/gfactory/glideinWMS/creation"
+sys.path.append(os.path.join(STARTUP_DIR,"lib"))
+sys.path.append(os.path.join(STARTUP_DIR,"../lib"))
+import cgWParams
+#import ldapMonitor
+import xmlParse
 
+############# need to delete later
+def addElement(elementParent,elementName,elementValue):
+#    params.data["entries"][elementParent]["attrs"][u"%s"%elementName]=xmlParse.OrderedDict({
+
+    elementParent[u"%s"%elementName]=xmlParse.OrderedDict({
+        "comment": None,
+        u"const": u"True",
+        u"parameter": u"True",
+        u"glidein_publish": u"True",
+        u"value": u"%s"%elementValue,
+        u"publish": u"True",
+        u"job_publish": u"True",
+        u"type": u"string"})
+
+#    newElement = ET.SubElement(
+#        elementParent,"attr",
+#        name=elementName,
+#        const="True",
+#        glidein_publish="True",
+#        job_publish="True",
+#        parameter="True",
+#        publish="True",
+#        type="string",
+#        value=elementValue)
+
+############# need to delete later
 def checkForGlobalElements():
-    attrNames = []
-    for i in glideinWMSxml.find("attrs"):
-        attrNames.append(i.get("name"))
-    if "VOS_USING_SE_VONAME_LOWERCASE" not in attrNames:
-        addElement("VOS_USING_SE_VONAME_LOWERCASE","",glideinWMSxml.find("attrs"))
-    if "VOS_USING_SE_BASEPATH" not in attrNames:
-        addElement("VOS_USING_SE_BASEPATH","",glideinWMSxml.find("attrs"))
-    if "VOS_USING_SE_OTHER_SUBDIR" not in attrNames:
-        addElement("VOS_USING_SE_OTHER_SUBDIR","",glideinWMSxml.find("attrs"))
+    if "VOS_USING_SE_VONAME_LOWERCASE" not in params.attrs.keys():
+        addElement(params.data["attrs"],"VOS_USING_SE_VONAME_LOWERCASE","")
+    if "VOS_USING_SE_BASEPATH" not in params.attrs.keys():
+        addElement(params.data["attrs"],"VOS_USING_SE_BASEPATH","")
+    if "VOS_USING_SE_OTHER_SUBDIR" not in params.attrs.keys():
+        addElement(params.data["attrs"],"VOS_USING_SE_OTHER_SUBDIR","")
 
-def removeDuplicates(priority1vo,priority2vo,priority3vo,priority3surl):
-    for i in priority3vo[:]:
-        if i in priority1vo or i in priority2vo:
-            priority3surl.pop(priority3vo.index(i))
-            priority3vo.remove(i)
-    for i in priority2vo[:]:
-        if i in priority1vo:
-            priority2vo.remove(i)
-    for i in priority3vo[:]:
-        if priority3vo.count(i) > 1:
-            if i.lower() != priority3surl[priority3vo.index(i)].split("/")[-1].lower():
-                priority3surl.pop(priority3vo.index(i))
-                priority3vo.remove(i)
+def createGenericSupportedVOList(supportedVOs):
+    mappedSupportedVOs = {
+        "ATLAS":"ATLAS",
+        "CDFEMI":"CDF",
+        "CMS":"CMS",
+        "CMSHTPC":"CMS",
+        "CMST1":"CMS",
+        "CMST2UCSD":"CMS",
+        "EngageVO":"Engage",
+        "EngageVOBigMem":"Engage",
+        "EngageVOHTPC":"Engage",
+        "EngageVOLong4d":"Engage",
+        "EngageVOVirt":"Engage",
+        "glowVO":"GLOW",
+        "GLUEX":"Gluex",
+        "HCC":"HCC",
+        "HCC4d":"HCC",
+        "HCCHTPC":"HCC",
+        "HCCLONG":"HCC",
+        "nanoHUB":"nanoHUB",
+        "NEBioGrid":"SBGrid",
+        "NEES":"NEES",
+        "NWICG":"NWICG",
+        "OSGVO":"OSG",
+        "OSGVOHTPC":"OSG",
+        "UCSDRok":"Engage"}
+    genericSupportedVOs = []
+    for i in supportedVOs.split(","):
+        if i in mappedSupportedVOs:
+            genericSupportedVOs.append(mappedSupportedVOs[i])
+        else:
+            print "Add support in mappedSVOs for " + i
+    return list(set(genericSupportedVOs))
 
+def createSurlDictionary():
+    surlDict = {}
+    f = open(config.locationSurlFile)
+    firstLine = f.readline()
+    ce = firstLine.split()[0]
+    vo = firstLine.split()[1]
+    surl = "/".join(firstLine.split()[2].split("/")[:-1])
+    if surl[-1] != "/":
+        surl = surl + "/"
+    seList = " ".join(firstLine.split()[1:])
+    for i in f:
+        vo = i.split()[1]
+        surl = "/".join(i.split()[2].split("/")[:-1]) 
+        if surl[-1] != "/":
+            surl = surl + "/"
+        if ce == i.split()[0]:
+            seList.append(vo + " " + surl)
+        else:
+            surlDict[ce] = seList
+            ce = i.split()[0]
+            seList = [vo + " " + surl]
+    f.close()
+    return surlDict
+
+############## need to delete later
+#def parseSurl():
+#    surlFile = open(config.locationSurlFile)
+#    surlLines = surlFile.readlines()
+#    surlFile.close()
+#
+############### need to delete later
+#def removeDuplicates(priority1vo,priority2vo,priority3vo,priority3surl):
+#    for i in priority3vo[:]:
+#        if i in priority1vo or i in priority2vo:
+#            priority3surl.pop(priority3vo.index(i))
+#            priority3vo.remove(i)
+#    for i in priority2vo[:]:
+#        if i in priority1vo:
+#            priority2vo.remove(i)
+#    for i in priority3vo[:]:
+#        if priority3vo.count(i) > 1:
+#            if i.lower() != priority3surl[priority3vo.index(i)].split("/")[-1].lower():
+#                priority3surl.pop(priority3vo.index(i))
+#                priority3vo.remove(i)
+#
+############### need to delete later
 def removeElement(elementList,elementName,elementParent):
     for i in elementList:
         if elementName in i.get("name"):
             elementParent.remove(i)
 
-surlFile = open(config.locationSurlFile)
-surlLines = surlFile.readlines()
-surlFile.close()
 
-glideinWMSxml = ET.parse(config.locationGlideinWMSxml)
+############################################################
+#
+# S T A R T U P
+# 
+############################################################
 
-entries = glideinWMSxml.findall("entries/entry")
+if __name__ == "__main__":
 
-SEs = {}
+    surlDict = createSurlDictionary()
+#    print surlDict
 
-mappedSVOs = {
-    'ATLAS':'ATLAS',
-    'CDFEMI':'CDF',
-    'CMS':'CMS',
-    'CMSHTPC':'CMS',
-    'CMST2UCSD':'CMS',
-    'EngageVO':'Engage',
-    'EngageVOBigMem':'Engage',
-    'EngageVOHTPC':'Engage',
-    'EngageVOLong4d':'Engage',
-    'EngageVOVirt':'Engage',
-    'glowVO':'GLOW',
-    'GLUEX':'Gluex',
-    'HCC':'HCC',
-    'HCC4d':'HCC',
-    'HCCHTPC':'HCC',
-    'HCCLONG':'HCC',
-    'nanoHUB':'nanoHUB',
-    'NEBioGrid':'SBGrid',
-    'NEES':'NEES',
-    'NWICG':'NWICG',
-    'OSGVO':'OSG',
-    'OSGVOHTPC':'OSG',
-    'UCSDRok':'Engage'}
+    params = cgWParams.GlideinParams("ZZZ",os.path.join(STARTUP_DIR,"web_base"),["ZZZ",config.locationGlideinWMSxml])
 
-VOs = ['accelerator','alice','argoneut','atlas','belle','cdf','cdms','cigi',
-    'cms','compbiogrid','csiu','dayabay','des','dosar','dream','dzero',
-    'engage','enmr.eu','fermirrid','fermilab','fermilablbne','fermitest',
-    'gcedu','gcvo','geant4','glow','gluex','gm2','gpn','gridunesp','grow',
-    'hcc','i2u2','icecube','ilc','lbne','ligo','map','mars','microboobe',
-    'minerva','miniboone','minos','mipp','mis','mu2e','nanohub','nebiogrid',
-    'nees','nersc','nova','numi','nwicg','nysgrid','ops','osg','osgedu',
-    'patriot','sbgrid','star','superbvo.org','suragrid','theory']
+    checkForGlobalElements()
 
-checkForGlobalElements()
-
-for i in entries:
-    if i.get("enabled") == "True":
-        attrs = i.findall("attrs/attr")
-        doEdit = None
-        for j in attrs:
-            if j.get("name") == "GLIDEIN_SEs":
-                SE = j.get("value")
-                doEdit = j
-            if j.get("name") == "GLIDEIN_Supported_VOs":
-                sVOs = j.get("value")
-
-        #skipped unless GLIDEIN_SEs is defined
-        if doEdit != None:
-            translatedSVOs = []
-            #translates VO list into a more generic VO list
-            for j in sVOs.split(','):
-                if j in mappedSVOs and mappedSVOs[j] not in translatedSVOs:
-                    translatedSVOs.append(mappedSVOs[j])
-
-            lowerVOs = []
-            lowerSurls = ''
-            baseVOs = []
-            baseSurls = ''
-            otherVOs = []
-            otherSurls = []
-
-            surlLines[0]
-
-            #parsing through surl file to find a match
-            for j in surlLines:
-                line = j.split()
-                if line[0] == SE and line[1] in translatedSVOs:
-                    temp = line[2].split('/')
-                    #lowercase check
-                    if temp[-2] == line[1].lower():
-                        lowerVOs.append(line[1])
-                        if lowerSurls == '':
-                            lowerSurls = "/".join(temp[0:-2])+"/"
-                    #base check
-                    elif temp[-2].lower() not in VOs:
-                        baseVOs.append(line[1])
-                        if baseSurls == '':
-                            baseSurls = "/".join(temp[0:-1])+"/"
-                    #other vos storage
+    for i in params.entries.keys():
+        #remove all references to this tool in glideinWMS.xml
+#        removeElement(attrs,"_SE_",i.find("attrs"))
+        #increment through entries with GLIDEIN_SEs defined
+        if params.entries[i]["enabled"] and params.entries[i]["attrs"].has_key("GLIDEIN_SEs"):
+##            print i
+            currentGlideinSE = params.entries[i]["attrs"]["GLIDEIN_SEs"]["value"]
+            currentGlideinSVOs = params.entries[i]["attrs"]["GLIDEIN_Supported_VOs"]["value"]
+#            print params.entries[i]["attrs"]["GLIDEIN_SEs"]["value"]
+            #check if this entries' GLIDEIN_SE is in the provided surl list
+            if currentGlideinSE in surlDict:
+                lowerVOs = []
+                lowerSurl = ""
+                baseVOs = []
+                baseSurl = ""
+                otherVOs = []
+                otherSurls = []
+                #translate glideinWMS.xml's GLIDEIN_Supported_VOs list to a more generic list
+                genericSupportedVOs = createGenericSupportedVOList(currentGlideinSVOs)
+#                print surlDict[params.entries[i]["attrs"]["GLIDEIN_SEs"]["value"]]
+                #base case has to be the first check
+                if len(currentGlideinSE) == 1:
+                    vo = surlDict[currentGlideinSE].split()[0]
+                    surl = surlDict[currentGlideinSE].split()[1]
+                    if vo in genericSupportedVOs:
+                        baseVOs.append(vo)
+                        baseSurl = surl
+                else:
+                    voList = []
+                    surlList = []
+                    for j in surlDict[currentGlideinSE]:
+                        vo = j.split()[0]
+                        surl = j.split()[1]
+                        if vo in genericSupportedVOs:
+                            voList.append(vo)
+                            surlList.append(surl)
+                    #true = base case, false = lower or other case
+                    if len(set(surlList)) == 1:
+                        baseVOs = voList
+                        baseSurl = surlList[0]
                     else:
-                        otherVOs.append(line[1])
-                        otherSurls.append("/".join(temp[0:-1])+"/")
+                        #increment through list of surl's for that entry
+                        for j in surlDict[currentGlideinSE]:
+#                    print j
+                            vo = j.split()[0]
+#                    print vo
+                            surl = j.split()[1]
+#                    print surl
+                            #checks if vo is in GLIDEIN_Supported_VOs
+                            if vo in genericSupportedVOs:
+                                #true = lower case, false = other case
+#                        print vo.lower()
+#                        print surl
+#                        print surl.split("/")[-1].lower()
+                                if vo.lower() == surl.split("/")[-2]:
+                                    lowerVOs.append(vo)
+                                    lowerSurl = surl.split(vo.lower())[0]
+                                    #handles the case where there are multiple surls for a vo
+                                    if vo in otherVOs:
+                                        otherSurls.remove(otherSurls[otherVOs.index(vo)])
+                                        otherVOs.remove(vo)
+#                            print surl + " --> " + lowerSurls
+                                elif vo not in lowerVOs:
+                                    #handles the case where there are multiple surls for a vo
+                                    if vo in otherVOs:
+                                        if vo.lower() == surl.split("/")[-2].lower():
+                                            otherSurls[otherVOs.index(vo)] = surl
+                                    else:
+                                        otherVOs.append(vo)
+                                        otherSurls.append(surl)
+#remove duplicates
 
-            removeDuplicates(lowerVOs,baseVOs,otherVOs,otherSurls)
+#                print "@@@@@@@@@@@@@@@@@@@@"
+#                print params.entries[i]["attrs"]["GLIDEIN_SEs"]["value"]
+#                print lowerSurl, lowerVOs
+#                print baseSurl, baseVOs
+#                print otherSurls, otherVOs
+#                print "@@@@@@@@@@@@@@@@@@@@"
 
-            removeElement(attrs,"_SE_",i.find("attrs"))
+                #adds SE location attribute to entry
+                if len(lowerVOs) > 0:
+                    addElement(params.data["entries"][i]["attrs"],"VOS_USING_SE_VONAME_LOWERCASE",",".join(lowerVOs))
+                    addElement(params.data["entries"][i]["attrs"],"GLIDEIN_SE_VONAME_LOWERCASE",lowerSurl)
+                if len(baseVOs) > 0:
+                    addElement(params.data["entries"][i]["attrs"],"VOS_USING_SE_BASEPATH",",".join(baseVOs))
+                    addElement(params.data["entries"][i]["attrs"],"GLIDEIN_SE_BASEPATH",baseSurl)
+                if len(otherVOs) > 0:
+                    addElement(params.data["entries"][i]["attrs"],"VOS_USING_SE_OTHER_SUBDIR",",".join(otherVOs))
+                for j in range(len(otherSurls)):
+                    addElement(params.data["entries"][i]["attrs"],"GLIDEIN_SE_PATH_"+otherVOs[j],otherSurls[j])
 
-            #adds SE location attribute to entry
-            if len(lowerVOs) > 0:
-                addElement("VOS_USING_SE_VONAME_LOWERCASE",",".join(lowerVOs),i.find("attrs"))
-                addElement("GLIDEIN_SE_VONAME_LOWERCASE",lowerSurls,i.find("attrs"))
-            if len(baseVOs) > 0:
-                addElement("VOS_USING_SE_BASEPATH",",".join(baseVOs),i.find("attrs"))
-                addElement("GLIDEIN_SE_BASEPATH",baseSurls,i.find("attrs"))
-            if len(otherVOs) > 0:
-                addElement("VOS_USING_SE_OTHER_SUBDIR",",".join(otherVOs),i.find("attrs"))
-            for j in range(len(otherSurls)):
-                addElement("GLIDEIN_SE_PATH_"+otherVOs[j],otherSurls[j],i.find("attrs"))
-
-glideinWMSxml.write("glideinWMS.xml.new")
+    params.save_into_file("glideinWMS.xml.new")
--- GOC-ITB/addStorageElements/config.py
+++ GOC-ITB/addStorageElements/config.py
@@ -1,2 +1,3 @@
-locationGlideinWMSxml = "/home/gfactory/glideinsubmit/glidein_v1_1/glideinWMS.xml"
-locationSurlFile = "/home/gfactory/tmortensen/addStorageElements/all-surl-spreadsheet-sorted.dat"
+#locationGlideinWMSxml = "/home/gfactory/glideinsubmit/glidein_v1_1/glideinWMS.xml"
+locationGlideinWMSxml = "glideinWMS.xml"
+locationSurlFile = "all-surl-spreadsheet-sorted.dat"
