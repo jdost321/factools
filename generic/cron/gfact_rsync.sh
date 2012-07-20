c87465dbb38759e80f1e220d6424150ca395d857
gfactory <gfactory@glidein-1.t2.ucsd.edu>
2012-07-19 21:04:46 -0700

    change src dirname in rsync tool

--- generic/cron/gfact_rsync.sh
+++ generic/cron/gfact_rsync.sh
@@ -3,10 +3,10 @@
 src_root=$1
 dest_root=$2
 
-base_client_log_dir="${src_root}/clientlogs"
-base_log_dir="${src_root}/glideinlogs"
+src_client_log_dir="${src_root}/clientlogs"
+src_log_dir="${src_root}/glideinlogs"
 dest_client_log_dir="${dest_root}/clientlogs"
 dest_log_dir="${dest_root}/glideinlogs"
 
-rsync -a ${base_client_log_dir}/ $dest_client_log_dir
-rsync -a --exclude '*.cifpk' --exclude '*.ftstpk' ${base_log_dir}/ $dest_log_dir
+rsync -a ${src_client_log_dir}/ $dest_client_log_dir
+rsync -a --exclude '*.cifpk' --exclude '*.ftstpk' ${src_log_dir}/ $dest_log_dir
