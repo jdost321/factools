e184a456acfc587680dc958ca2ea60f34eecf761
Jeff Dost <jdost@ucsd.edu>
2012-09-14 15:05:00 -0700

    add gfact_queu_clean.sh

---
+++ generic/cron/gfact_queue_clean.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source ~/.bash_profile
+source  /etc/profile.d/condor.sh
+now=`date +%s`
+age=$1
+sleep=1200
+
+pushd $GLIDEIN_FACTORY_DIR > /dev/null
+schedds=`head -n 1 glideinWMS.xml | sed 's/^.*schedd_name="\([^"]\+\)".*/\1/' | tr , ' '`
+popd > /dev/null
+
+for schedd in $schedds;do
+    condor_rm -name $schedd -const "qdate <= $((now - 3600*24*age))"
+done
+
+sleep $sleep
+
+for schedd in $schedds;do
+    condor_rm -forcex -name $schedd -const "qdate <= $((now - 3600*24*age))"
+done
