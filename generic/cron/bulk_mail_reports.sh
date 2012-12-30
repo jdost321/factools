496c5d226fdc8284ef0e813390088a9bc0a3fa67
Jeff Dost <jdost@ucsd.edu>
2012-12-29 18:41:37 -0800

    make mail scripts generic and limit an argument

---
+++ generic/cron/bulk_mail_reports.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source ~/.bash_profile
+
+script_dir=`dirname $0`
+mail_report=${script_dir}/../libexec/mail_report.sh
+conf=$1
+
+while read line; do
+    # only consider non empty lines that aren't comments
+    if [ -n "$line" ] && ! echo "$line" | grep -q '^#'; then
+        $mail_report $line
+    fi
+done < $conf
