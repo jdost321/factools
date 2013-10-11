519412449f5d9258ee493282ed1067243b73f66b
Luis Emiro Linares Garcia <luis.linares@cern.ch>
2013-10-11 19:26:48 +0200

    Correction of path in downtimes mail script

--- generic/cron/check_downtimes.sh
+++ generic/cron/check_downtimes.sh
@@ -3,7 +3,9 @@
 source ~/.bash_profile
 
 script_dir=`dirname $0`
-tool="${script_dir}/../bin/is_entry_in_downtime"
+#tool="${script_dir}/../bin/is_entry_in_downtime"
+tool="${script_dir}/../bin/new_entries_in_downtime"
 
-$tool all 
+#$tool all 
+$tool --send
 
