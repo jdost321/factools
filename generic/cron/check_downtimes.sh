d39a372e1196090ab966e437172982625e1dfb56
Luis Emiro Linares Garcia <luis.linares@cern.ch>
2013-10-14 14:02:22 +0200

    change downtimes cronjob

--- generic/cron/check_downtimes.sh
+++ generic/cron/check_downtimes.sh
@@ -2,7 +2,7 @@
 
 source ~/.bash_profile
 
-script_dir=`dirname $0`
+script_dir=$(dirname $0)
 #tool="${script_dir}/../bin/is_entry_in_downtime"
 tool="${script_dir}/../bin/new_entries_in_downtime"
 
