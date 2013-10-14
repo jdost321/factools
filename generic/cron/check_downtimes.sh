cb7ffcaa5faea9005e73296598fad5f681357fad
Luis Emiro Linares Garcia <luis.linares@cern.ch>
2013-10-14 17:14:06 +0200

    Downtimes scripts tweaks

--- generic/cron/check_downtimes.sh
+++ generic/cron/check_downtimes.sh
@@ -6,7 +6,7 @@ script_dir=$( cd $(dirname $0); pwd -P )
 #tool="${script_dir}/../bin/is_entry_in_downtime"
 tool="${script_dir}/../bin/new_entries_in_downtime"
 
-echo $tool
 #$tool all 
 #$tool --send
+$tool 
 
