ea9dfa3adc7210204a511f7555ad551782d1b35e
Luis Emiro Linares Garcia <luis.linares@cern.ch>
2013-10-15 00:46:18 +0200

    Added updates-only option.

--- generic/cron/check_downtimes.sh
+++ generic/cron/check_downtimes.sh
@@ -3,9 +3,7 @@
 source ~/.bash_profile
 
 script_dir=$( cd $(dirname $0); pwd -P )
-#tool="${script_dir}/../bin/is_entry_in_downtime"
-tool="${script_dir}/../bin/new_entries_in_downtime"
+tool="${script_dir}/../bin/is_entry_in_downtime"
 
-#$tool all 
-#$tool --send
+$tool --updates --send all 
 
