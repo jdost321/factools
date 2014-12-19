8195b89281e5c6650b112e2c4c90d93672e7156e
Jeff Dost <jdost@ucsd.edu>
2014-12-18 19:33:31 -0800

    make mail_report.sh rpm compatible

--- generic/libexec/mail_report.sh
+++ generic/libexec/mail_report.sh
@@ -5,22 +5,18 @@ if [ -z "$GLIDEIN_MAIL_REPLY_TO" ]; then
     exit 1
 fi
 
-# Fail if src dir is not set
-if [ -z "$GLIDEIN_SRC_DIR" ]; then
-    exit 1
-fi
-
-# Fail if monitor url is not set
-if [ -z "$GLIDEIN_MON_URL" ]; then
-    exit 1
-fi
-
 if [ -n "GLIDEIN_FACTORY_NAME" ];then
     factory_name="$GLIDEIN_FACTORY_NAME "
 fi
 
-tools_path="${GLIDEIN_SRC_DIR}/factory/tools"
-source="$GLIDEIN_MON_URL"
+# check if rpm install
+if [ -e /usr/bin/analyze_entries ];then
+  tools_path=/usr/bin
+else
+  tools_path="${GLIDEIN_SRC_DIR}/factory/tools"
+  src_flag="--source $GLIDEIN_MON_URL"
+fi
+
 date=$(eval date +%m-%d-%Y)
 
 mode=$1
@@ -38,22 +34,22 @@ case "$mode" in
     ae)
         script=analyze_entries
         if [ "$target" = factory ]; then
-            cmd="${tools_path}/${script} --source $source -x $interval -s waste  -l strt,10 --nb;${tools_path}/${script} --source $source -x $interval -s waste -m;${tools_path}/${script} --source $source -x $interval -s waste"
+            cmd="${tools_path}/${script} $src_flag -x $interval -s waste  -l strt,10 --nb;${tools_path}/${script} $src_flag -x $interval -s waste -m;${tools_path}/${script} $src_flag -x $interval -s waste"
         else
-            cmd="${tools_path}/${script} --source $source -x $interval -s waste -f $target"
+            cmd="${tools_path}/${script} $src_flag -x $interval -s waste -f $target"
         fi
     ;;
     aq)
         script=analyze_queues
         if [ "$target" = factory ]; then
-            cmd="${tools_path}/${script} --source $source -x $interval -s rundiff -z -m;${tools_path}/${script} --source $source -x $interval -s rundiff -z"
+            cmd="${tools_path}/${script} $src_flag -x $interval -s rundiff -z -m;${tools_path}/${script} $src_flag -x $interval -s rundiff -z"
         else
-            cmd="${tools_path}/${script} --source $source -x $interval -s rundiff -f $target -z"
+            cmd="${tools_path}/${script} $src_flag -x $interval -s rundiff -f $target -z"
         fi
     ;;
     af)
         script=analyze_frontends
-        cmd="${tools_path}/${script} --source $source -x $interval -s unmatched -f $target -z"
+        cmd="${tools_path}/${script} $src_flag -x $interval -s unmatched -f $target -z"
     ;;
 esac
 
