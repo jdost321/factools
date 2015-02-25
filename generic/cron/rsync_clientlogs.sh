a3402dec2883511c3c6634661851bba78610fbfb
Jeff Dost <jdost@ucsd.edu>
2015-02-25 13:52:40 -0800

    add client log rsync script

---
+++ generic/cron/rsync_clientlogs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+script_dir=`dirname $0`
+conf=${script_dir}/../../etc/rsync_clientlogs.conf
+
+while read line; do
+  if [ -n "$line" ] && ! echo "$line" | grep -q '^#'; then
+    set -- $line
+    user=$1
+    shift
+    src=$1
+    shift
+    dest=$1
+    shift
+
+    su $user -c "/usr/bin/rsync -e 'ssh -c blowfish' --include '/entry_*/' --include '/entry_*/job.*.out' --include '/entry_*/job.*.err' --exclude '*' --min-size 1 -axv $src $dest >/tmp/rsync_${user}.log"
+  fi
+done < $conf
