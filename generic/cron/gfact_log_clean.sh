3c0140da59b9dd9fd4f907effb24f055d065a97a
Jeff Dost <jdost@ucsd.edu>
2015-04-24 11:50:14 -0700

    update gfact_log_clean.sh to be rpm compatible

--- generic/cron/gfact_log_clean.sh
+++ generic/cron/gfact_log_clean.sh
@@ -2,7 +2,13 @@
 
 age=$1 # how many days to keep logs
 src_root=$2
-src_client_log_dir="${src_root}/clientlogs"
-src_log_dir="${src_root}/glideinlogs"
+src_client_log_dir="${src_root}/client"
+src_log_dir="${src_root}/server"
+
+# not an rpm based factory, try non-rpm locations
+if [ ! -e "$src_client_log_dir" ];then
+    src_client_log_dir="${src_root}/clientlogs"
+    src_log_dir="${src_root}/glideinlogs"
+fi
 
 /usr/sbin/tmpwatch -d -m $((age * 24)) $src_client_log_dir $src_log_dir
