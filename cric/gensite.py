e4a833ac4ab461a4db0f7d906aa90567399a533b
Edita Kizinevic <edita.kizinevic@cern.ch>
2019-02-28 16:34:03 +0100

    Add submit_attrs

--- cric/gensite.py
+++ cric/gensite.py
@@ -12,9 +12,6 @@ if response.status_code != 200:
     sys.exit(-1)
 sites = response.json()
 
-#https://cms-cric.cern.ch/api/cms/computeunit/query/?json
-#sites = json.load(open("admin.json"))
-
 result = {}
 
 for _, siteinfo in sites.items():
@@ -22,8 +19,11 @@ for _, siteinfo in sites.items():
     if site not in result:
         result[site] = {}
     for entry in siteinfo["glideinentries"]:
-        if entry["gridtype"] == "cream":
+        grid_type = entry["gridtype"]
+        if grid_type == "cream":
             continue
+        glidein_cpus = entry["GLIDEIN_CPUS"]
+        glidein_max_mem = entry["GLIDEIN_MaxMemMBs"]
         gatekeeper = entry["gatekeeper"]
         if " " in gatekeeper:
             gatekeeper = gatekeeper.split(" ")[1]
@@ -31,17 +31,18 @@ for _, siteinfo in sites.items():
         result[site][gatekeeper]["work_dir"] = entry["workdir"]
         result[site][gatekeeper]["attrs"] = {}
         result[site][gatekeeper]["attrs"]["GLIDEIN_REQUIRED_OS"] = entry["GLIDEIN_REQUIRED_OS"]
-        result[site][gatekeeper]["attrs"]["GLIDEIN_CPUS"] = entry["GLIDEIN_CPUS"]
-        result[site][gatekeeper]["attrs"]["GLIDEIN_MaxMemMBs"] = entry["GLIDEIN_MaxMemMBs"] or 2500
-        # Category 4
+        result[site][gatekeeper]["attrs"]["GLIDEIN_CPUS"] = glidein_cpus
+        result[site][gatekeeper]["attrs"]["GLIDEIN_MaxMemMBs"] = glidein_max_mem
         result[site][gatekeeper]["rsl"] = entry["rsl"]
         result[site][gatekeeper]["attrs"]["GLIDEIN_CMSSite"] = entry["GLIDEIN_CMSSite"]
         result[site][gatekeeper]["attrs"]["GLIDEIN_Site"] = site
-#        result[site][gatekeeper]["attrs"]["GLIDEIN_Supported_VOs"] = "CMS"
-#        result[site][gatekeeper]["trust_domain"] = "grid"
+        try:
+            if grid_type == "condor" and int(glidein_cpus) > 1:
+                result[site][gatekeeper]["submit_attrs"] = {}
+                result[site][gatekeeper]["submit_attrs"]["+maxMemory"] = glidein_max_mem
+                result[site][gatekeeper]["submit_attrs"]["+xcount"] = glidein_cpus
+        except ValueError:
+            pass
 
 with open("2category.yml", "w") as outfile:
     yaml.safe_dump(result, outfile, default_flow_style=False)
-
-pprint.pprint(result)
-print len(result)
