290bb070776c25c677c75eb1d863cf1aac6aa958
Marco Mascheroni <marco.mascheroni@cern.ch>
2020-02-24 17:08:06 +0100

    Run pylint over the osg.py module

--- cric/OSG/osg.py
+++ cric/OSG/osg.py
@@ -1,17 +1,19 @@
-import os
+""" Allows to retrieve information from the OSG collector and generate the factory cml file
+"""
+
+from __future__ import print_function
+
 import sys
-import yaml
-import urllib2
-import htcondor
 import fractions
-import xmltodict
-import collections
+
+import htcondor
 
 from cfg.config import OSG_COLLECTOR, OSG_YAML, OSG_WHITELIST, OSG_DEFAULT
-from libs.utils import entry_stub, GLIDEIN_Supported_VOs_map, default_attr, MergeError, get_attr_str, write_to_file, update, get_yaml_file_info, write_to_yaml_file, get_submit_attr_str, write_to_xml_file
+from libs.utils import entry_stub, GLIDEIN_Supported_VOs_map, MergeError, get_attr_str, update, \
+     get_yaml_file_info, write_to_yaml_file, get_submit_attr_str, write_to_xml_file
 
 
-def get_VOs(allowed_vos):
+def get_vos(allowed_vos):
     """This function converts the list of VO from the collector to the frontend ones
 
     Args:
@@ -20,14 +22,14 @@ def get_VOs(allowed_vos):
     Returns:
         set: The set of frontend VOs to add to the configuration GLIDEIN_SupportedVOs
     """
-    VOs = set()
-    for VO in allowed_vos:
-        if VO in GLIDEIN_Supported_VOs_map:
-            VOs.add(GLIDEIN_Supported_VOs_map[VO])
+    vos = set()
+    for vorg in allowed_vos:
+        if vorg in GLIDEIN_Supported_VOs_map:
+            vos.add(GLIDEIN_Supported_VOs_map[vorg])
         else:
-            print VO + " VO is not in GLIDEIN_Supported_VOs_map"
+            print(vorg + " VO is not in GLIDEIN_Supported_VOs_map")
 
-    return VOs
+    return vos
 
 
 def get_information(host):
@@ -44,80 +46,84 @@ def get_information(host):
         dict: A resource dictionary whose keys are the resources ('cedar', 'UCHICAGO', 'NMSU')
             and its values are the needed information to create the entry. For example:
 
-            {'hosted-ce32.grid.uchicago.edu': {'DEFAULT_ENTRY': {'attrs': {'GLIDEIN_CPUS': {'value': 16L},
-                                                                           'GLIDEIN_MaxMemMBs': {'value': 163840L},
-                                                                           'GLIDEIN_Max_Walltime': {'value': 171000L},
-                                                                           'GLIDEIN_ResourceName': {'value': 'NEMO'},
-                                                                           'GLIDEIN_Site': {'value': 'OSG_US_UWM_NEMO'},
-                                                                           'GLIDEIN_Supported_VOs': {'value': 'OSGVO'}},
-                                                                 'gridtype': 'condor',
-                                                                 'submit_attrs': {'+maxMemory': 163840L,
-                                                                                  '+maxWallTime': 2880L,
-                                                                                  '+xcount': 16L}}}}
+            {'hosted-ce32.grid.uchicago.edu':
+                     {'DEFAULT_ENTRY': {'attrs': {'GLIDEIN_CPUS': {'value': 16L},
+                                                  'GLIDEIN_MaxMemMBs': {'value': 163840L},
+                                                  'GLIDEIN_Max_Walltime': {'value': 171000L},
+                                                  'GLIDEIN_ResourceName': {'value': 'NEMO'},
+                                                  'GLIDEIN_Site': {'value': 'OSG_US_UWM_NEMO'},
+                                                  'GLIDEIN_Supported_VOs': {'value': 'OSGVO'}},
+                                        'gridtype': 'condor',
+                                        'submit_attrs': {'+maxMemory': 163840L,
+                                                         '+maxWallTime': 2880L,
+                                                         '+xcount': 16L}}}}
     """
     collector = htcondor.Collector(host)
     ces = collector.query(htcondor.AdTypes.Schedd)
     result = {}
     entry = "DEFAULT_ENTRY"
-    for ce in ces:
+    for celem in ces:
         # Only focus on the hsotedCEs for the time being
-        if not ce["Name"].lower().startswith("hosted-ce"):
+        if not celem["Name"].lower().startswith("hosted-ce"):
             continue
-        if "OSG_ResourceGroup" in ce:
-            site = ce["OSG_ResourceGroup"]
+        if "OSG_ResourceGroup" in celem:
+            site = celem["OSG_ResourceGroup"]
             if site:
                 if site not in result:
                     result[site] = {}
-                gatekeeper = ce["Name"].lower()
+                gatekeeper = celem["Name"].lower()
                 result[site][gatekeeper] = {}
                 resource = ""
-                if "OSG_Resource" in ce:
-                    resource = ce["OSG_Resource"]
-                if "OSG_ResourceCatalog" in ce:
-                    VOs = set()
+                if "OSG_Resource" in celem:
+                    resource = celem["OSG_Resource"]
+                #TODO The following "if" should be put in a function to make pylint happy
+                if "OSG_ResourceCatalog" in celem:
+                    vos = set()
                     memory = sys.maxint
                     walltime = sys.maxint
-                    CPUs = "" 
-                    for OSG_ResourceCatalog in ce["OSG_ResourceCatalog"]:
-                        if "AllowedVOs" in OSG_ResourceCatalog:
-                            if len(VOs) == 0:
-                                VOs = get_VOs(OSG_ResourceCatalog["AllowedVOs"])
+                    cpus = ""
+                    for osg_catalog in celem["OSG_ResourceCatalog"]:
+                        if "AllowedVOs" in osg_catalog:
+                            if len(vos) == 0:
+                                vos = get_vos(osg_catalog["AllowedVOs"])
                             else:
-                                VOs = VOs.intersection(get_VOs(OSG_ResourceCatalog["AllowedVOs"]))
-                        if "Memory" in OSG_ResourceCatalog:
-                            memory = min(memory, OSG_ResourceCatalog["Memory"])
-                        if "MaxWallTime" in OSG_ResourceCatalog:
-                            walltime = min(walltime, OSG_ResourceCatalog["MaxWallTime"])
-                        if "CPUs" in OSG_ResourceCatalog:
-                            if CPUs == "" :
-                                CPUs = OSG_ResourceCatalog["CPUs"]
+                                vos = vos.intersection(get_vos(osg_catalog["AllowedVOs"]))
+                        if "Memory" in osg_catalog:
+                            memory = min(memory, osg_catalog["Memory"])
+                        if "MaxWallTime" in osg_catalog:
+                            walltime = min(walltime, osg_catalog["MaxWallTime"])
+                        if "CPUs" in osg_catalog:
+                            if cpus == "":
+                                cpus = osg_catalog["CPUs"]
                             else:
-                                CPUs = fractions.gcd(CPUs, OSG_ResourceCatalog["CPUs"])
-                    result[site][gatekeeper][entry] = {}
-                    result[site][gatekeeper][entry]["gridtype"] = "condor"
-                    result[site][gatekeeper][entry]["attrs"] = {}
-                    result[site][gatekeeper][entry]["attrs"]["GLIDEIN_Site"] = {"value": resource}
+                                cpus = fractions.gcd(cpus, osg_catalog["CPUs"])
+                    # Assigning this to an entry dict vriable to shorten the line
+                    edict = result[site][gatekeeper][entry]
+                    edict = {}
+                    edict["gridtype"] = "condor"
+                    edict["attrs"] = {}
+                    edict["attrs"]["GLIDEIN_Site"] = {"value": resource}
                     if resource:
-                        result[site][gatekeeper][entry]["attrs"]["GLIDEIN_ResourceName"] = {"value": site}
-                    if len(VOs) > 0:
-                        result[site][gatekeeper][entry]["attrs"]["GLIDEIN_Supported_VOs"] = {"value": ",".join(VOs)}
+                        edict["attrs"]["GLIDEIN_ResourceName"] = {"value": site}
+                    if len(vos) > 0:
+                        edict["attrs"]["GLIDEIN_Supported_vos"] = {"value": ",".join(vos)}
                     else:
-                        print gatekeeper + " CE does not have VOs"
-                    result[site][gatekeeper][entry]["submit_attrs"] = {}
-                    if CPUs != "":
-                        result[site][gatekeeper][entry]["attrs"]["GLIDEIN_CPUS"] = {"value": CPUs}
-                        result[site][gatekeeper][entry]["submit_attrs"]["+xcount"] = CPUs
+                        print(gatekeeper + " CE does not have VOs")
+                    edict["submit_attrs"] = {}
+                    if cpus != "":
+                        edict["attrs"]["GLIDEIN_CPUS"] = {"value": cpus}
+                        edict["submit_attrs"]["+xcount"] = cpus
                     if walltime != sys.maxint:
                         glide_walltime = walltime * 60 - 1800
-                        result[site][gatekeeper][entry]["attrs"]["GLIDEIN_Max_Walltime"] = {"value": glide_walltime}
-                        result[site][gatekeeper][entry]["submit_attrs"]["+maxWallTime"] = walltime
+                        edict["attrs"]["GLIDEIN_Max_Walltime"] = {"value": glide_walltime}
+                        edict["submit_attrs"]["+maxWallTime"] = walltime
                     if memory != sys.maxint:
-                        result[site][gatekeeper][entry]["attrs"]["GLIDEIN_MaxMemMBs"] = {"value": memory}
-                        result[site][gatekeeper][entry]["submit_attrs"]["+maxMemory"] = memory
+                        edict["attrs"]["GLIDEIN_MaxMemMBs"] = {"value": memory}
+                        edict["submit_attrs"]["+maxMemory"] = memory
                 else:
-                    print gatekeeper + " CE does not have OSG_ResourceCatalog attribute"
+                    print(gatekeeper + " CE does not have OSG_ResourceCatalog attribute")
             else:
-                print gatekeeper + " CE does not have OSG_ResourceGroup attribute"
+                print(gatekeeper + " CE does not have OSG_ResourceGroup attribute")
 
     return result
 
@@ -132,58 +138,71 @@ def get_entries_configuration(data):
         str: The factory cml file as a string
     """
     entries_configuration = ""
-    for site, site_information in data.items():
-        for ce, ce_information in site_information.items():
+    for _, site_information in data.items():
+        for celem, ce_information in site_information.items():
             for entry, entry_information in ce_information.items():
                 entry_configuration = entry_information
                 entry_configuration["entry_name"] = entry
-                entry_configuration["attrs"]["GLEXEC_BIN"] = {"value": "NONE"} # Can we get this information?
-                entry_configuration["attrs"]["GLIDEIN_REQUIRED_OS"] = {"comment" : "This value has been hardcoded", "value": "any"} # Can we get this information?
-                entry_configuration["gatekeeper"] = ce + " " + ce + ":9619" # Probably we can use port from attribute AddressV1 or CollectorHost
+                # Can we get these information (next two keys)?
+                entry_configuration["attrs"]["GLEXEC_BIN"] = {"value": "NONE"}
+                entry_configuration["attrs"]["GLIDEIN_REQUIRED_OS"] = (
+                    {"comment" : "This value has been hardcoded", "value": "any"}
+                )
+                # Probably we can use port from attribute AddressV1 or CollectorHost
+                entry_configuration["gatekeeper"] = celem + " " + celem + ":9619"
                 entry_configuration["rsl"] = ""
                 entry_configuration["attrs"] = get_attr_str(entry_configuration["attrs"])
                 if  "submit_attrs" in entry_configuration:
-                    entry_configuration["submit_attrs"] = get_submit_attr_str(entry_configuration["submit_attrs"])
+                    entry_configuration["submit_attrs"] = (
+                        get_submit_attr_str(entry_configuration["submit_attrs"])
+                    )
                 else:
                     entry_configuration["submit_attrs"] = ""
                 entries_configuration += entry_stub % entry_configuration
 
     return entries_configuration
-    
+
 
 def merge_yaml():
     """Merges different yaml file and return the corresponding resource dictionary
 
     Three different yam files are merged. First we read the factory white list/override file that
-    contains the list of entries operators want to generate, with the parameters they want to override.
+    contains the list of entries operators want to generate, with the parameters they want to
+    override.
     Then the yaml generated from the OSG collector and the default yam file are read.
-    For each entry an all the operator information are "updated" with the information coming from the
-    collector first, and from the default file next.
+    For each entry an all the operator information are "updated" with the information coming from
+    the collector first, and from the default file next.
 
     Returns:
-        dict: a dict similar to the one returned by ``get_information``, but with all the defaults and
-            the operators overrides in place (only whitelisted entries are returned).
+        dict: a dict similar to the one returned by ``get_information``, but with all the defaults
+            and the operators overrides in place (only whitelisted entries are returned).
     """
     out = {}
-    out = get_yaml_file_info(OSG_YAML, 5)
-    OSG_information = get_yaml_file_info(OSG_WHITELIST, 6)
+    out = get_yaml_file_info(OSG_WHITELIST, 5)
+    osg_info = get_yaml_file_info(OSG_YAML, 6)
     default_information = get_yaml_file_info(OSG_DEFAULT, 7)
     for site, site_information in out.items():
-        if site not in OSG_information:
-            print("You put %s in the whitelist file, but the site is not present in the collector" % site)
+        if site not in osg_info:
+            print("You put %s in the whitelist file, but the site is not present in the collector"
+                  % site)
             raise MergeError(8)
-        for ce, ce_information in site_information.items():
-            if ce not in OSG_information[site]:
-                print ("Working on whitelisted site %s: cannot find ce %s in the generated OSG.yaml" % (site, ce))
+        for celem, ce_information in site_information.items():
+            if celem not in osg_info[site]:
+                print ("Working on whitelisted site %s: cant find ce %s in the generated OSG.yaml"
+                       % (site, celem))
                 raise MergeError(9)
             for entry, entry_information in ce_information.items():
-                if entry_information == None:
-                    out[site][ce][entry] = OSG_information[site][ce]["DEFAULT_ENTRY"]
-                    entry_information = out[site][ce][entry]
+                if entry_information is None:
+                    out[site][celem][entry] = osg_info[site][celem]["DEFAULT_ENTRY"]
+                    entry_information = out[site][celem][entry]
                 else:
-                    update(entry_information, OSG_information[site][ce]["DEFAULT_ENTRY"], overwrite=False)
-                update(entry_information, default_information["DEFAULT_SITE"]["DEFAULT_GETEKEEPER"]["DEFAULT_ENTRY"], overwrite=False)
-
+                    update(entry_information, osg_info[site][celem]["DEFAULT_ENTRY"],
+                           overwrite=False)
+                update(
+                    entry_information,
+                    default_information["DEFAULT_SITE"]["DEFAULT_GETEKEEPER"]["DEFAULT_ENTRY"],
+                    overwrite=False
+                )
     return out
 
 
@@ -199,11 +218,11 @@ def main():
     entries_configuration = get_entries_configuration(result)
     # Write the factory configuration file on the disk
     write_to_xml_file("entries.xml", entries_configuration)
-    
+
 
 if __name__ == "__main__":
     try:
         main()
-    except MergeError as me:
-        print("Error! " + MergeError.codes_map[me.code])
-        sys.exit(me.code)
+    except MergeError as merr:
+        print("Error! " + MergeError.codes_map[merr.code])
+        sys.exit(merr.code)
