d29aade4097d9fa99fffe8931f0c12b18cba9ac1
Jeff Dost <jdost@ucsd.edu>
2023-09-23 18:39:49 -0700

    improve mailer error checking

--- libexec/mail_report.sh
+++ libexec/mail_report.sh
@@ -34,43 +34,89 @@ case "$mode" in
     ae)
         script=analyze_entries
         if [ "$target" = factory ]; then
-            cmd="${tools_path}/${script} $src_flag -x $interval -s waste  -l wst,25 --nb;${tools_path}/${script} $src_flag -x $interval -s waste -m;${tools_path}/${script} $src_flag -x $interval -s waste"
+            out1=$(${tools_path}/${script} $src_flag -x $interval -s waste  -l wst,25 --nb)
+            if [ $? -ne 0 ]; then
+              echo "failed to generate factory ae sec 1"
+              exit 1
+            fi
+            out2=$(${tools_path}/${script} $src_flag -x $interval -s waste -m)
+            if [ $? -ne 0 ]; then
+              echo "failed to generate factory ae sec 2"
+              exit 1
+            fi
+            out3=$(${tools_path}/${script} $src_flag -x $interval -s waste)
+            if [ $? -ne 0 ]; then
+              echo "failed to generate factory ae sec 3"
+              exit 1
+            fi
+            out=$(echo -e "${out1}\n${out2}\n${out3}")
         else
-            cmd="${tools_path}/${script} $src_flag -x $interval -s waste -f $target"
+            out=$(${tools_path}/${script} $src_flag -x $interval -s waste -f $target)
+            if [ $? -ne 0 ]; then
+              echo "failed to generate $target ae"
+              exit 1
+            fi
         fi
     ;;
     aq)
         script=analyze_queues
         if [ "$target" = factory ]; then
-            cmd="${tools_path}/${script} $src_flag -x $interval -s rundiff -z -m;${tools_path}/${script} $src_flag -x $interval -s rundiff -z"
+            out1=$(${tools_path}/${script} $src_flag -x $interval -s rundiff -z -m)
+            if [ $? -ne 0 ]; then
+              echo "failed to generate factory aq sec 1"
+              exit 1
+            fi
+            out2=$(${tools_path}/${script} $src_flag -x $interval -s rundiff -z)
+            if [ $? -ne 0 ]; then
+              echo "failed to generate factory aq sec 2"
+              exit 1
+            fi
+            out=$(echo -e "${out1}\n${out2}")
         else
-            cmd="${tools_path}/${script} $src_flag -x $interval -s rundiff -f $target -z"
+            out=$(${tools_path}/${script} $src_flag -x $interval -s rundiff -f $target -z)
+            if [ $? -ne 0 ]; then
+              echo "failed to generate $target aq"
+              exit 1
+            fi
         fi
     ;;
     aqw)
         script=analyze_queues
-        cmd="${tools_path}/${script} $src_flag -x $interval -s wait -z -m;${tools_path}/${script} $src_flag -x $interval -s wait -z"
+        out1=$(${tools_path}/${script} $src_flag -x $interval -s wait -z -m)
+        if [ $? -ne 0 ]; then
+          echo "failed to generate aqw sec 1"
+          exit 1
+        fi
+        out2=$(${tools_path}/${script} $src_flag -x $interval -s wait -z)
+        if [ $? -ne 0 ]; then
+          echo "failed to generate aqw sec 2"
+          exit 1
+        fi
+        out=$(echo -e "${out1}\n${out2}")
     ;;
     aqr)
         script=analyze_queues
-        cmd="${tools_path}/${script} $src_flag -x $interval -s %rundiff -z -m;${tools_path}/${script} $src_flag -x $interval -s %rundiff -z"
+        out1=$(${tools_path}/${script} $src_flag -x $interval -s %rundiff -z -m)
+        if [ $? -ne 0 ]; then
+          echo "failed to generate aqr sec 1"
+          exit 1
+        fi
+        out1=$(${tools_path}/${script} $src_flag -x $interval -s %rundiff -z)
+        if [ $? -ne 0 ]; then
+          echo "failed to generate aqr sec 2"
+          exit 1
+        fi
+        out=$(echo -e "${out1}\n${out2}")
     ;;
     af)
         script=analyze_frontends
-        cmd="${tools_path}/${script} $src_flag -x $interval -s unmatched -f $target -z"
+        out=$(${tools_path}/${script} $src_flag -x $interval -s unmatched -f $target -z)
+        if [ $? -ne 0 ]; then
+          echo "failed to generate $target af"
+          exit 1
+        fi
     ;;
 esac
-
-#echo "$cmd"
-out=`eval $cmd`
-#echo "$out"
-retval=$?
-#echo $retval
-
-# for now only suppress emailing if error occurs for frontend
-if [ $target != "factory" -a $retval -ne 0 ]; then
-    exit $retval
-fi
 
 # hack to get more specific subjects for aq reports
 if [ "$mode" = "aqw" ];then
@@ -84,7 +130,6 @@ if [ "$interval" = 2 ]; then
 else
     subject="${factory_name}Factory ${script} report for last ${interval}h on ${date}"
 fi
-
 
 /usr/sbin/sendmail -oi -t <<EOF
 To: ${emails}
