2b7daad11d4b9cc3f67308f8940be7f9d73f489b
Jeff Dost <jdost@ucsd.edu>
2023-10-24 20:49:49 -0700

    update log clean to work without tmpwatch

--- cron/gfact_log_clean.sh
+++ cron/gfact_log_clean.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 age=$1 # how many days to keep logs
-src_client_log_dir=/var/log/gwms-factory/client
-src_log_dir=/var/log/gwms-factory/server
-stage_dir=/var/lib/gwms-factory/web-area/stage
 
-/usr/sbin/tmpwatch -d -m $((age * 24)) $src_client_log_dir $src_log_dir $stage_dir
+dirs="/var/log/gwms-factory/client /var/log/gwms-factory/server /var/lib/gwms-factory/web-area/stage"
+
+for d in $dirs; do
+  find $d -type f -mtime +${age} | xargs rm -f
+done
